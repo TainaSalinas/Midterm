1269f6700dcfa3c38d6dd0529c32a2f0787e76d0
TainaSalinas <TainaSalinas@users.noreply.github.com>
2022-10-21 13:03:24 -0400

    Adding Title/End Screen

--- Assets/BeginGame.cs
+++ Assets/BeginGame.cs
@@ -14,7 +14,7 @@ public class BeginGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.anyKey)
         {
             SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
         }
